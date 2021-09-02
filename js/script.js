787b302b5481adccfd2dd166d194b75ec971331a
NurulIslam-EEE <ni163034@gmail.com>
2021-09-02 18:07:04 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -2,37 +2,62 @@
 
 const inputField = document.getElementById('input-field');
 const inputButton = document.getElementById('input-btn');
+const errorMessage = document.getElementById('error-message');
+const cardContainer = document.getElementById('card-container');
+const itemFound = document.getElementById('item-found');
 
+// -----------------load data--------------
 
 const loadData = () => {
-    const inputValue = inputField.value;
-    const url = `http://openlibrary.org/search.json?q=${inputValue}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displayData(data.docs))
+  //----------clear previous data------------
+  errorMessage.innerText = '';
+  itemFound.innerText = '';
+  cardContainer.innerHTML = '';
+  const inputValue = inputField.value;
+  if (inputValue === "") {
+    errorMessage.innerText = 'Search field can not be empty';
+    return;
+  }
+
+  const url = `https://openlibrary.org/search.json?q=${inputValue}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => displayData(data.docs));
 }
-const parent = document.getElementById('card-container');
 
-const displayData = (array) => {
+//--------display data--------
 
-    array.forEach(element => {
-        const div = document.createElement('div');
-        div.classList.add('card-size');
-        div.setAttribute('width', '100px')
-        console.log(div);
-        div.innerHTML = `
+const displayData = (booksDetails) => {
+  if (booksDetails.length === 0) {
+    itemFound.innerText = '';
+    errorMessage.innerText = 'No result found';
+    inputField.value = '';
+
+  } else {
+    errorMessage.innerText = '';
+    booksDetails.forEach(element => {
+      const div = document.createElement('div');
+      div.classList.add('card-sizing');
+      // div.setAttribute('width', '100px')
+      div.innerHTML = `
         <div class="card">
-    <img src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="...">
+    <img class="img-sizing" src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${element.title}</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text">Author:${element.author_name ? element.author_name[0] : 'N/A'}</p>
+      <p class="card-text">Publisher: ${element.publisher ? element.publisher[0] : 'N/A'}</p>
+      <p class="card-text">Publish:  ${element.publish_date ? element.publish_date[0] : 'N/A'}</p>
+      <p class="card-text">First Publish: ${element.first_publish_year}</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+      <small class="text-muted">see details</small>
     </div>
-  </div>`
-        parent.appendChild(div);
+  </div>`;
+      cardContainer.appendChild(div);
     });
+    itemFound.innerText = `about ${booksDetails.length} result found`;
+    inputField.value = '';
 
+  }
 }
 
